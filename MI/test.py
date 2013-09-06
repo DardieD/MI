9b60a7b5d97008565597b060ab1daac9a7b4c617
DardieD <salunkeshanu91@gmail.com>
2013-09-06 22:42:09 +0530

    Cleaning up old files

--- MI/test.py
+++
@@ -1,8 +0,0 @@
-from django.conf import settings
-settings.configure()
-
-from django.core.mail import EmailMessage
-email = EmailMessage('Hello', 'World', to=['salunkeshanu91@gmail.com'])
-email.send()
-
-
